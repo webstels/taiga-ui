f15e4b54dd65bed980eadb2cf01e66ceb2ce2a16
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-01-20 16:32:31 +0500

    chore: fix build

--- projects/kit/components/slider/slider.module.ts
+++ projects/kit/components/slider/slider.module.ts
@@ -7,8 +7,8 @@ import {
     TuiRepeatTimesModule,
 } from '@taiga-ui/cdk';
 import {TuiFormatNumberPipeModule} from '@taiga-ui/core';
-import {TuiSliderComponent} from '@taiga-ui/kit/components';
 
+import {TuiSliderComponent} from './slider.component';
 import {TuiSliderOldComponent} from './slider-old.component';
 
 @NgModule({
